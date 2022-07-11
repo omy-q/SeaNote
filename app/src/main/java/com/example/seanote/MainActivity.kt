f0985337b7d4fe300aac9b79fdbac04577ccd2d2
omy-q <olya_aparina@mail.ru>
2022-07-11 11:52:46 +0300

    add test activity for test view

--- app/src/main/java/com/example/seanote/MainActivity.kt
+++ app/src/main/java/com/example/seanote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.seanote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toTestViewScreen()
+    }
+
+    private fun toTestViewScreen() {
+        val intent = Intent(this, TestViewActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/seanote/TestViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.seanote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TestViewActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_test)
+    }
+}
