da5ebe980b2d5428e89205a34ba1449f3016bcad
omy-q <olya_aparina@mail.ru>
2022-07-13 17:30:26 +0300

    create db with room

--- app/src/main/java/com/example/seanote/storage/SeaNoteDataBase.kt
+++ app/src/main/java/com/example/seanote/storage/SeaNoteDataBase.kt
@@ -2,12 +2,15 @@ package com.example.seanote.storage
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 
-@Database(
-    entities = [SeaNoteEntity::class],
-    version = 1
-)
+@Database(entities = [SeaNoteEntity::class], version = 1)
+@TypeConverters(NoteTextConverter::class)
 abstract class SeaNoteDataBase : RoomDatabase() {
     abstract val noteDao: SeaNoteDao
+
+    companion object {
+        const val tableName = "SeaNoteEntity"
+    }
 }
--- app/src/main/java/com/example/seanote/storage/SeaNoteEntity.kt
+++ app/src/main/java/com/example/seanote/storage/SeaNoteEntity.kt
@@ -3,7 +3,16 @@ package com.example.seanote.storage
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = SeaNoteDataBase.tableName)
 data class SeaNoteEntity(
-    @PrimaryKey val id: Int
+    @PrimaryKey
+    val idNote: Int,
+    val date: String,
+    val title: String,
+    val text: List<String>,
+    val idTextType: Int,
+    val idBackground: Int,
+    val idFontType: Int,
+    val isFavorite: Int,
+    val isToDoList: Int
 )
